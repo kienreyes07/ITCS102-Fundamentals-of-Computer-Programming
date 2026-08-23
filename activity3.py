5c1049d43475144bbf57744490a22b511753eff6
kienreyes07 <kien.reyes07@gmail.com>
2026-08-23 18:17:39 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,47 @@
+#My first program
+
+#integer
+age = 18
+sibling = 2
+birth_year = 2007
+pet = 1
+
+#string 
+name = "Kien Aaron C. Reyes"
+talent = "duck cover and beg,"
+address = "Ibabang Dupay, University Site."
+pet_name = "Cannie"
+
+#float
+temp = 35.4
+height = 5.7
+weight = 42.3
+chance = 0.01
+
+#bool
+dumb = True
+madly = True
+is_hungry = True
+is_tired = False
+
+
+
+
+print("My name is", name, ",", "Im currently", age, "years old", "born in October", birth_year, "has", pet, "dog pet named", pet_name, "and", sibling, "siblings living in", address)
+
+print("On my 1st day of college, I showed them that I've master", talent, "Im", height, "ft", "and I", "weight", weight, "kilograms.")
+
+print("I don't have a fever right now, I feel great even though its already 3am in the morning, studying at night is fun and calming despite the cold and having a", temp, "celcius temperature.")
+
+print("This is nothing compared to the coldness I received from her. I know I have a", chance, "chance of winning her heart yet I still chose to pursue her.")
+
+print("")
+
+print("Am I dumb?", dumb) 
+print("Am I madly in love with her?", madly)
+print("Am I hungry thinking about it?", is_hungry)
+print("Am I tired of trying?", is_tired)
+
+
+
+
