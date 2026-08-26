2133c70a6fc9b672587af733e363fcb37989e355
kienreyes07 <kien.reyes07@gmail.com>
2026-08-27 01:30:23 +0800

    Add files via upload

---
+++ codechallenge1.py
@@ -0,0 +1,6 @@
+#Code Challenge1
+
+#str
+apartment = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t*\t\t\t\t\t\t*\t*\n\t\t\t\t\t\t\t\t\t*\t*\t    Hi I'm Kien Aaron C. Reyes\t\t*\t*\n\t\t\t\t\t\t\t\t\t*\t*\t\t From BSIT 1-B\t\t\t*\t*\n\t\t\t\t\t\t\t\t\t*\t*\t\t\t\t\t\t*\t*\n\t\t\t\t\t\t\t\t\t*\t*\t\t\t\t\t\t*\t*\n\t\t\t\t\t\t\t\t\t*\t*\t\t\t\t\t\t*\t*\n\t\t\t\t\t\t\t\t\t*\t*\t*\t*\t*\t*\t*\t*\t*\n\t\t\t\t\t\t\t\t\t*\t*\t*\t*\t*\t*\t*\t*\t*"
+
+print(apartment)
