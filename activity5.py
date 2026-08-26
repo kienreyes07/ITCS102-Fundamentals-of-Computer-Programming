96c4a4606fbaadc59f37d24bec3516230feee7c4
kienreyes07 <kien.reyes07@gmail.com>
2026-08-27 02:49:34 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,21 @@
+#Learning Operators
+
+a = 27
+b = 5
+
+sum = a+b
+product = a*b
+dif = a-b
+divide = a/b
+delete = a//b
+exponent = a**b
+modulo = a%b
+
+print("The sum of",a, "and",b, "is",sum)
+print("The product of",a, "and",b, "is",product)
+print("The difference of",a, "and",b, "is",dif)
+print("The division of",a, "and",b, "is",divide)
+print("The floor deletion of",a, "and",b, "is",delete)
+print("The exponent of",a, "and",b, "is",exponent)
+print("The modulo of",a, "and",b, "is",modulo)
+
