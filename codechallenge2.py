ffd8be6e7fa3ade848455044934752d5e112d845
kienreyes07 <kien.reyes07@gmail.com>
2026-08-27 02:25:49 +0800

    Add files via upload

---
+++ codechallenge2.py
@@ -0,0 +1,57 @@
+#ph denomination
+
+a = 1000
+b = 500
+c = 200
+d = 100
+e = 50
+f = 20
+g = 10
+h = 5
+i = 1
+
+money = 19863
+dif = money
+
+sum = dif // a
+dif = dif % a
+
+
+sum2 = dif // b
+dif = dif % b
+
+sum1 = dif // c
+dif = dif % c 
+
+sum3 = dif // d
+dif = dif % d
+
+sum4 = dif // e
+dif = dif % e
+
+sum5 = dif // f
+dif = dif % f
+
+sum6 = dif // g
+dif = dif % g
+
+sum7 = dif // h
+dif = dif % h
+
+sum8 = dif // i
+dif = dif % i
+
+
+
+
+print("Money to Deposit -->", money)
+
+print("1000:",sum)
+print("500:",sum2)
+print("200:",sum1)
+print("100:",sum3)
+print("50 :",sum4)
+print("20 :",sum5)
+print("10 :",sum6)
+print("5 :",sum7)
+print("1 :",sum8)
