293b2c81a47d2b3f8477ed8185b3486b30129816
kienreyes07 <kien.reyes07@gmail.com>
2026-08-29 00:24:52 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,12 @@
+#Reyes's activity4
+
+#escape sequence
+# \n
+# \t
+# \r
+# \b
+# \\
+# \"
+
+phrase = ("If \r\"someone else\b\b\b\b\b can do it, why do it?\". \n\t\t\\This kind of mindset kills our potential")
+print(phrase)
