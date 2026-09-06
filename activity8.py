d90fd5cf05dcf1953342c1a5e2fefb2040e7aaa7
kienreyes07 <kien.reyes07@gmail.com>
2026-09-06 13:19:12 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,84 @@
+#activity 8 reyes kien
+
+total = ""
+hobbies = ""
+
+b = (input("Enter Your Name ---> "))
+total += b + ", "	
+
+a = (input("Enter Your Age ---> "))
+total += b + ", "
+
+c = (input("Enter Your Address ---> "))
+total += c + ", "
+
+d = (input("Enter Your Birthdate ---> "))
+total += d + ", "
+
+e = (input("Enter Your Civil Status ---> "))
+total += e + ", "
+
+f = (input("Enter Your Bio ---> "))
+total += f + ", "
+
+g = (input("Enter Your Working Site ---> "))
+total += g + ", "
+
+h = (input("Users That Follows You ---> "))
+total += h + ", "
+
+i = (input("Users That You Follow ---> "))
+total += i + ", "
+
+j = (input("Enter Your Talent ---> "))
+total += j + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+a1 = (input("Enter Hobbies ---> "))
+hobbies += a1 + ", "
+
+print("")
+
+
+
+print("================User's Info================")
+
+print("Name: ", b)
+print("Age: ", a)
+print("Address: ", c)
+print("Birthdate: ", d)
+print("Civil Status: ", e)
+print("Bio: ", f)
+print("Works at: ", g)
+print("Followers: ", h)
+print("Following: ", i)
+print("Talent: ", j)
+print("Hobbies: {", hobbies,"}")
+
+print("=============Follow To Interact=============")
