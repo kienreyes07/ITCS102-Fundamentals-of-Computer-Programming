58958fe590426709f81739a224b082154542e00f
kienreyes07 <kien.reyes07@gmail.com>
2026-09-06 13:18:33 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,44 @@
+#activity 7 reyes kien
+
+total = 0
+
+
+a = eval(input("Enter Number ---> "))
+total = total +a
+
+b = eval(input("Enter Number---> "))
+total += b
+
+c = eval(input("Enter Number ---> "))
+total += c 
+
+d = eval(input("Enter Number ---> "))
+total += d
+
+e = eval(input("Enter Number ---> "))
+total += e
+
+f = eval(input("Enter Number ---> "))
+total += f
+
+g = eval(input("Enter Number ---> "))
+total += g
+
+h = eval(input("Enter Number ---> "))
+total += h 
+
+i = eval(input("Enter Number ---> "))
+total += i 
+
+j = eval(input("Enter Number ---> "))
+total += j
+
+
+print("")
+
+
+print("=============ACCOUNT BALANCE=============")
+
+print("Current Total Addition: ", total)
+
+print("=============END OF BALANCE=============")
